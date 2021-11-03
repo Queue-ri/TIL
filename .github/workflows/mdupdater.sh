6e98d7c9e4f9fa67097d3bca7f823c63e9af19d0
Queue-ri <qriositylog@gmail.com>
2021-11-03 23:01:45 +0900

    ci: Skip if there's no document today

--- .github/workflows/mdupdater.sh
+++ .github/workflows/mdupdater.sh
@@ -4,6 +4,7 @@
 # mdupdater by Queue-ri
 
 GREEN='\033[0;32m'
+YELLOW='\033[1;33m'
 NOCOLOR='\033[0m'
 
 TODAY=`date "+%Y-%m-%d"`
@@ -15,10 +16,27 @@ FOLDER="../../md/date/${YEAR}/${MONTH}"
 echo -e "\n${GREEN}** MDUPDATER by Queue-ri"
 echo -e "** configuring ${TODAY}.md...${NOCOLOR}"
 
-find ../../docs -name "*.md" > filepath_list # all md path
+find ../../docs -name "*.md" > filepath_prelist # all md path
+touch filepath_list
 
-mkdir -p $FOLDER
-echo "# 📚 ${TODAY}" > $FILE
+while read filepath; do
+    filedate=`grep "created_date" $filepath | cut -f2 -d " " | head -1` # should be a single space, not ":".
+    fileupdate=`grep "updated_date" $filepath | cut -f2 -d " " | head -1`
+
+    if [[ "$filedate" == "$TODAY" || "$fileupdate" == "$TODAY" ]]; then
+        echo $filepath >> filepath_list
+    fi
+done < filepath_prelist
+
+if [[ -s filepath_list ]]; then
+    mkdir -p $FOLDER
+    echo "# 📚 ${TODAY}" > $FILE
+else
+    echo -e "${YELLOW}** skip mdupdater: no docs have been written today.\n${NOCOLOR}"
+    rm filepath_prelist
+    rm filepath_list
+    exit 0
+fi
 
 while read filepath; do
     pagepath=`echo ${filepath%.*} | sed 's/docs/featured/'`
@@ -33,5 +51,6 @@ while read filepath; do
     fi
 done < filepath_list
 
+rm filepath_prelist
 rm filepath_list
-echo -e "${GREEN}** configuration done successfully!${NOCOLOR}"
+echo -e "${GREEN}** configuration done successfully!\n"
