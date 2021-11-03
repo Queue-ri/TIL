f1e364ab7315ad3d64ad23990073bec72efadadb
Queue-ri <qriositylog@gmail.com>
2021-11-04 00:10:29 +0900

    ci: Add auto commit & push script

---
+++ .github/workflows/commit_push.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# 21-11-03
+# commit_push by Queue-ri
+
+GREEN='\033[0;32m'
+NOCOLOR='\033[0m'
+
+TODAY=`date "+%Y-%m-%d"`
+
+echo -e "${GREEN}** working on git repository...${NOCOLOR}\n"
+
+cd ..
+cd ..
+git add --all
+git status
+git commit -m "ci(docs): Auto commit ${TODAY}.md"
+git push origin main
+
+echo -e "${GREEN}** ALL SET ;) See you!${NOCOLOR}"
