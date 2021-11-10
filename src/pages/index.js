a91bcb4f6abc186fad2f442b35b7179874fb5566
Queue-ri <qriositylog@gmail.com>
2021-11-10 13:33:45 +0900

    perf: Improve font rendering of navbar/hero title

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,8 +15,8 @@ function HomepageHeader() {
         <div className={clsx('banner__img', styles.bannerImg)}></div>
       </div>
       <div className={clsx('container', styles.container)}>
-        <h1 className="hero__title">{siteConfig.title}</h1>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
+        <h1 className={clsx("hero__title", styles.bannerTitle)}>{siteConfig.title}</h1>
+        <p className={clsx("hero__subtitle", styles.bannerSubTitle)}>{siteConfig.tagline}</p>
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
