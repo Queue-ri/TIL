9f6a30d4d2078a4ae8681e90ba710b1319d24b52
Queue-ri <qriosity.dev@gmail.com>
2025-08-13 23:04:08 +0900

    chore: Swizzle `CodeBlock` & `DocItem` (#147)
    내장 컴포넌트 원본

---
+++ src/theme/CodeBlock/Line/index.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import clsx from 'clsx';
+import LineToken from '@theme/CodeBlock/Line/Token';
+import styles from './styles.module.css';
+// Replaces '\n' by ''
+// Historical code, not sure why we even need this :/
+function fixLineBreak(line) {
+  const singleLineBreakToken =
+    line.length === 1 && line[0].content === '\n' ? line[0] : undefined;
+  if (singleLineBreakToken) {
+    return [{...singleLineBreakToken, content: ''}];
+  }
+  return line;
+}
+export default function CodeBlockLine({
+  line: lineProp,
+  classNames,
+  showLineNumbers,
+  getLineProps,
+  getTokenProps,
+}) {
+  const line = fixLineBreak(lineProp);
+  const lineProps = getLineProps({
+    line,
+    className: clsx(classNames, showLineNumbers && styles.codeLine),
+  });
+  const lineTokens = line.map((token, key) => {
+    const tokenProps = getTokenProps({token});
+    return (
+      <LineToken key={key} {...tokenProps} line={line} token={token}>
+        {tokenProps.children}
+      </LineToken>
+    );
+  });
+  return (
+    <span {...lineProps}>
+      {showLineNumbers ? (
+        <>
+          <span className={styles.codeLineNumber} />
+          <span className={styles.codeLineContent}>{lineTokens}</span>
+        </>
+      ) : (
+        lineTokens
+      )}
+      <br />
+    </span>
+  );
+}
